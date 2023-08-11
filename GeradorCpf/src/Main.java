710b01df814d31042e23b0e328ceda95c8a6e4e0
Lopes Diego (CaP/ETS) <lod7ca@bosch.com>
2023-08-11 08:18:28 -0300

    final

--- GeradorCpf/src/Main.java
+++ GeradorCpf/src/Main.java
@@ -6,7 +6,37 @@ public class Main {
     static Random random = new Random();
 
     public static void validarCPF(){
+        int validadorD1 = 0, validadorD2 = 0, d1, d2, cont = 0;
         System.out.println("Digite o CPF que deseja validar: ");
+        String cpf = entrada.next().replace(".", "").replace("-", "");
+        for (int i = 10; i >= 2; i--) {
+            char caracter = cpf.charAt(cont);
+            int numero = Integer.parseInt(Character.toString(caracter));
+            validadorD1 += (numero * i);
+            validadorD2 += (numero * (i+1));
+            cont++;
+        }
+
+        d1 = 11 - (validadorD1 % 11);
+
+        validadorD2 += d1 * 2;
+        d2 = 11 - (validadorD2 % 11);
+
+        if(d1 > 9){
+            d1 = 0;
+        }
+        if(d2 > 9){
+            d2 = 0;
+        }
+
+        char c1 = cpf.charAt(9);
+        char c2 = cpf.charAt(10);
+
+        int n1 = Integer.parseInt(Character.toString(c1));
+        int n2 = Integer.parseInt(Character.toString(c2));
+
+        String resultado = (d1 == n1) && (d2 == n2) ? "CPF válido!" : "CPF inválido!";
+        System.out.println(resultado);
     }
 
     public static void gerarCPF(){
@@ -18,7 +48,6 @@ public class Main {
             cpf.append(strNumero);
         }
         for (int i = 10; i >= 2; i--) {
-            System.out.println(i);
             char caracter = cpf.charAt(cont);
             int numero = Integer.parseInt(Character.toString(caracter));
             validadorD1 += (numero * i);
@@ -61,6 +90,9 @@ public class Main {
                     case 2 -> gerarCPF();
                     case 3 -> System.out.println("Finalizando Programa");
                     default -> System.out.println("Número inválido");
+                }
+                if (opcao == 3){
+                    break;
                 }
             } catch (Exception e) {
                 System.out.printf("Erro: %s\n", e);
